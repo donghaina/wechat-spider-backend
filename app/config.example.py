acfa26d0324ca6aecdd311d51ba75fa8d33ce597
taosang1992 <taosang1992@gmail.com>
2018-11-16 16:51:36 +0800

    修改配置文件

--- app/config.example.py
+++ app/config.example.py
@@ -3,5 +3,5 @@ from datetime import timedelta
 DEBUG = True
 SECRET_KEY = 'horizon'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://spider:7ujm<KI*9ol.@www.aikepler.com:3306/wechat_spider'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/wechat_spider'
 SEND_FILE_MAX_AGE_DEFAULT = timedelta(seconds=1)
